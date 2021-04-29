4d97ba2ed26ff7ff43767dcc51e70aa20159a53d
thaknal <krzysiekpnq50@gmail.com>
2021-04-29 19:46:58 +0200

    Lab2 A)

--- src/pl/lublin/wsei/java/cwiczenia/IsPalindromeTest.java
+++ src/pl/lublin/wsei/java/cwiczenia/IsPalindromeTest.java
@@ -1,4 +1,22 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import pl.lublin.wsei.java.cwiczenia.mylib.StringFun;
+
+import java.util.Scanner;
+
 public class IsPalindromeTest {
-}
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        do {
+            System.out.print("Podaj tekst do sprawdzenia (0, jak Ci się znudzi): ");
+            String tekst = scn.next();
+            if (tekst.equals("0")) break;
+            System.out.print("Tekst: " + tekst + " " +
+                    (StringFun.isPalindrome(tekst) ? "" : "nie ") +
+                    "jest palindromem");
+        } while (true);
+
+        }
+    }
+
