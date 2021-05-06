5077302d5485ca4fc94bc29292eef6cd1ec1cc98
thaknal <krzysiekpnq50@gmail.com>
2021-05-06 14:25:31 +0200

    Lab2 E)

--- src/pl/lublin/wsei/java/cwiczenia/IsPalindromeTest.java
+++ src/pl/lublin/wsei/java/cwiczenia/IsPalindromeTest.java
@@ -1,6 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
 import pl.lublin.wsei.java.cwiczenia.mylib.StringFun;
 
 import java.util.Scanner;
@@ -8,16 +8,17 @@ import java.util.Scanner;
 public class IsPalindromeTest {
 
     public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
         do {
-            System.out.print("Podaj tekst do sprawdzenia (0, jak Ci się znudzi): ");
-            String tekst = scn.next();
-            if (tekst.equals("0")) break;
-            System.out.print("Tekst: " + tekst + " " +
-                    (StringFun.isPalindrome(tekst) ? "" : "nie ") +
-                    "jest palindromem");
-        } while (true);
-
+            System.out.print("\nWprowadź tekst do wylosowania litery," +
+                    "aby zakonczyć wprowadź - \"0\"");
+            String sentence = scanner.nextLine();
+            if (sentence.equals("0")) break;
+            System.out.print(StringFun.Shuffle(sentence));
         }
-    }
+        while (true);
+        return;
 
+    }
+}
--- src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
@@ -2,15 +2,31 @@ package pl.lublin.wsei.java.cwiczenia.mylib;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Random;
+
 public class StringFun {
 
-    public static boolean isPalindrome(String tekst) {
-        StringBuilder textToCompare = new StringBuilder();
-        tekst = tekst.toLowerCase().trim();
-        for (int i = 0; i < tekst.length(); i++) {
-            if (Character.isWhitespace(tekst.charAt(i))) continue;
-            textToCompare.append(tekst.charAt(i));
+    public static Object Shuffle(String sentence) {
+        Random random = new Random();
+
+        int lenght;
+        int lenght2 = sentence.length();
+        char charSentence[];
+        String shuffledSentence = "";
+        int randomizedNumber;
+
+        for (int i = 0; i < lenght2; i++) {
+            lenght = sentence.length();
+            randomizedNumber = random.nextInt(lenght);
+            if (randomizedNumber == 0) randomizedNumber++;
+            charSentence = sentence.toCharArray();
+
+            shuffledSentence = shuffledSentence + charSentence[randomizedNumber - 1];
+
+            sentence = sentence.substring(0, randomizedNumber - 1) + sentence.substring(randomizedNumber, lenght);
+
+            return shuffledSentence;
         }
-        return textToCompare.toString().equals(textToCompare.reverse().toString());
+        return null;
     }
 }
